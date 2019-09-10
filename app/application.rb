cc0971ab58e8462d01739c97a5fc0b161ef0842b
Gregory Losco <gtlosco@gmail.com>
2019-09-10 22:24:14 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,9 +3,10 @@ class Application
   def call(env)
     resp = Rack::Response.new
     
-    if Time 
+    if Time.between?(0..6)
       resp.write "Good Morning!"
     else 
+      resp.write "Good Afternoon!"
   end
   
 end
